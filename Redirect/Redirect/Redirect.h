a469baa02fa52175f5b652f0decd085f1a8a61a5
chen <815807765@qq.com>
2019-07-23 10:40:06 +0800

    add

---
+++ Redirect/Redirect/Redirect.h
@@ -0,0 +1,17 @@
+//
+//  Redirect.h
+//  Redirect
+//
+//  Created by CYQ on 2019/7/23.
+//  Copyright © 2019 CYQ. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface Redirect : UIView
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ Redirect/Redirect/Redirect.m
@@ -0,0 +1,21 @@
+//
+//  Redirect.m
+//  Redirect
+//
+//  Created by CYQ on 2019/7/23.
+//  Copyright © 2019 CYQ. All rights reserved.
+//
+
+#import "Redirect.h"
+
+@implementation Redirect
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
